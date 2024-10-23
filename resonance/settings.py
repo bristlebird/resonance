3c2ef41e5fe7e6e6046a4408bfd2b31a39b974df
Adam Green <adam@bristlebird.com>
2024-10-23 18:28:09 +0000

    import environment variables

--- resonance/settings.py
+++ resonance/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!90%6!bpi7f4ztjzao+-mjg3p_(%8tn8@qnui+-vmv_14&f#v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
