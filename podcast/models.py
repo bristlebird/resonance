0ff56fea6557c103a1cb4f9b8ab631da0415190b
Adam Green <adam@bristlebird.com>
2024-10-23 21:59:58 +0000

    add podcast database model

--- podcast/models.py
+++ podcast/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+# class Post(models.Model):
+class Podcast(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    creator = models.ForeignKey(
+        # User, on_delete=models.CASCADE, related_name="blog_posts"
+        # User, on_delete=models.CASCADE, related_name="podcast_shows, podcast_episodes"
+        User, on_delete=models.CASCADE, related_name="podcast_shows"
+    )
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    copyright = models.CharField(max_length=200)
+    keywords = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    owner_name = models.CharField(max_length=200)
+    owner_email = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
