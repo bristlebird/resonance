12c0d3f1acde6f5f256f9f895cc043677b1b38c2
Adam Green <adam@bristlebird.com>
2024-10-31 20:13:43 +0000

    update podcast model, add languages & categories

--- podcast/models.py
+++ podcast/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from podcast.categories import CATEGORY_CHOICES
+# from podcast.languages import LANGUAGE_CHOICES
+
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-# class Post(models.Model):
 class Podcast(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,11 +17,15 @@ class Podcast(models.Model):
     )
     author = models.CharField(max_length=200)
     description = models.TextField()
+    # primary_category = models.CharField(choices=categories.CATEGORY_CHOICES)
+    # secondary_category = models.CharField(choices=categories.CATEGORY_CHOICES)
     copyright = models.CharField(max_length=200)
     keywords = models.CharField(max_length=200)
     website = models.CharField(max_length=200)
+    # spoken_language = models.CharField(choices=languages.LANGUAGE_CHOICES, default="en")
     owner_name = models.CharField(max_length=200)
     owner_email = models.CharField(max_length=200)
+    explicit_content_warning = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
