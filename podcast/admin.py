8e0e34cf09aef894b8cf8ef04192aece0871e654
Adam Green <adam@bristlebird.com>
2024-11-05 14:55:10 +0000

    add summernote admi panel package

--- podcast/admin.py
+++ podcast/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Podcast, Episode
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Podcast)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
+
 
 # Register your models here.
-admin.site.register(Podcast)
 admin.site.register(Episode)
--- resonance/urls.py
+++ resonance/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from podcast import views as podcast_views
 
 urlpatterns = [
-    # path('podcast/', podcast_views.my_podcast, name='podcast'),
-    path('', include("podcast.urls"), name='podcast-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('', include("podcast.urls"), name='podcast-urls'),
 ]
