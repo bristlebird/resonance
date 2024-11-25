204e03913bd1777806d23bff1e95c4ebe0657d59
Adam Green <adam@bristlebird.com>
2024-11-25 22:20:20 +0000

    set debug false for deploy

--- resonance/settings.py
+++ resonance/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEVELOPMENT")
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['8000-bristlebird-resonance-3ry7mcb5oez.ws.codeinstitute-ide.net', '.herokuapp.com',]
