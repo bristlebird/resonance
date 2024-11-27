4f212c137277886f2d7120b0a2e936f066f24711
Adam Green <adam@bristlebird.com>
2024-11-27 18:03:02 +0000

    set cloudinary image upload folder in podcast model

--- podcast/models.py
+++ podcast/models.py
@@ -20,7 +20,7 @@ class Podcast(models.Model):
     author = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
     # artwork = models.CharField(max_length=200, blank=True, default='path/to/image/file')
-    artwork = CloudinaryField('image', default='placeholder')
+    artwork = CloudinaryField('image', default='placeholder', folder='resonance/images')
     # primary_category = models.CharField(choices=categories.CATEGORY_CHOICES)
     # secondary_category = models.CharField(choices=categories.CATEGORY_CHOICES)
     copyright = models.CharField(max_length=200, blank=True)
